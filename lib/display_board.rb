6633c1f26675bbbcc410146398c4bb424ad3612f
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-09-27 21:31:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -15,7 +15,8 @@ display_board(board)
 board[4]= "X"
 display_board(board)
 
-board[4] => board[6]= "X"
+board[4] = "X"
+board[5] + board[6] = "X"
 display_board(board)
 
 # rows[0]).to eq("   |   |   ")
