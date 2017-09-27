1189cdff1ce0997cb50ebbf7787da6d25d88bd9d
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-09-27 22:28:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,6 @@
 
 def display_board(rows)
 
-  a= "X"
-  b= "O"
-  
   rows[0]= "   |   |   "
   puts rows[0]
   rows[1]= "-----------"
@@ -16,13 +13,21 @@ def display_board(rows)
   puts rows[3]
   rows[4]= "   |   |   "
   puts rows[4]
+
 end
+
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 display_board(board)
 
-board[0] = "X"
-display_board(board)
+def display_board(rows)
+  puts ("#{board[0]}  | #{board[1]} | #{board[2]}")
+  puts "-----------"
+  puts ("#{board[3]}  | #{board[4]} | #{board[5]}")
+  puts "-----------"
+  puts ("#{board[6]}  | #{board[7]} | #{board[8]}")
+end
+
 
 # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 # display_board(board)
