9da67b33083670be7c3381a0118b71fbbfd3a5a3
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-09-27 21:12:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,9 +10,7 @@ def display_board(board)
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
+display_board(board)
 
 board[4]= "X"
 
