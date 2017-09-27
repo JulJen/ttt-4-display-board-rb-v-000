89bd38027bb8d820ecfa2e6d98aa5dd8ce488079
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-09-27 22:17:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board
+def display_board(rows)
 
-  rows[0]= ("#{board[0]}  | #{board[1]} | #{board[2]}")
-  rows[1]= "-----------"
-  rows[2]= ("#{board[3]}  | #{board[4]} | #{board[5]}")
-  rows[3]= "-----------"
-  rows[4]= ("#{board[6]}  | #{board[7]} | #{board[8]}")
+  rows[0]= "   |   |   "
+  puts row[0]
+
+  # rows[0]= ("#{board[0]}  | #{board[1]} | #{board[2]}")
+  # rows[1]= "-----------"
+  # rows[2]= ("#{board[3]}  | #{board[4]} | #{board[5]}")
+  # rows[3]= "-----------"
+  # rows[4]= ("#{board[6]}  | #{board[7]} | #{board[8]}")
 end
 
 # def display_board(board)
